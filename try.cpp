4dcf8118ede629fc0474838047ff6670f18756f9
nasr <nasrghaleb713@gmail.com>
2024-12-06 10:00:41 +0300

    adding cpp file by the second user (alsaidi) and fill it

---
+++ try.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main (){
+
+
+
+char name[20] ; 
+cout<<  " enter your first name : \n " ; 
+cin.getline(name , 20 , '\n') ;  
+cout << name << endl;
+
+
+return 0 ; 
+}
